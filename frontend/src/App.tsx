889aec1a2a2a189936d8839c9e1be38f0c8754ae
MozesCsaszar <Mozes.Csaszar@endava.com>
2025-11-27 16:21:46 +0200

    feat: add React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import ProductsPage from "./components/ProductsPage/ProductsPage";
 
 function App() {
   return (
     <>
-
+      <ProductsPage></ProductsPage>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
